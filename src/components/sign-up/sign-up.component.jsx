d0afb51601189029e87a30de43b588f74b50224c
Jeffrey <renjeffreyy@gmail.com>
2020-01-18 04:52:34 -0800

    Firebase and begging of Sign up component, storing user data in firebase

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
