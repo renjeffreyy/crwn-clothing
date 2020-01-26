78ec423a85dd696a922929f302a442166c756af8
Jeffrey <renjeffreyy@gmail.com>
2020-01-25 20:44:44 -0800

    implemented cart reducer and made it so user can toggle cart to display or be hidden

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,10 +5,11 @@ import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo'/>
@@ -28,11 +29,17 @@ const Header = ({currentUser}) => (
             }
             <CartIcon />
         </div>
+        {
+        hidden? null :<CartDropdown />
+        }
+
     </div>    
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user:{ currentUser}, cart:{hidden}}) => ({
+    currentUser,
+    hidden
+
 });
 
 export default connect(mapStateToProps)(Header);
